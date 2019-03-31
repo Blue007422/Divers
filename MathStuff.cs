d22ffd5a7ce49207d8eb79b81e326cf076239839
ValentinOp <valentin.copin@gmail.com>
2019-04-01 01:33:55 +0200

    Update MathStuff.cs

--- MathStuff.cs
+++ MathStuff.cs
@@ -1,27 +1,45 @@
 
 static int PGCD(int a, int b)
-  {
-      int c;
-      int d;
-      int r;
+{
+    int c;
+    int d;
+    int r;
 
-      if(a > b)
-      {
-          c = a;
-          d = b;
-      }
-      else
-      {
-          d = a;
-          c = b;
-      }
+    if(a > b)
+    {
+        c = a;
+        d = b;
+    }
+    else
+    {
+        d = a;
+        c = b;
+    }
 
-      while(d != 0 && c%d != 0 && d != 1)
-      {
-          r = c%d;
-          c = d;
-          d = r;
-      }
+    while(d != 0 && c%d != 0 && d != 1)
+    {
+        r = c%d;
+        c = d;
+        d = r;
+    }
 
-      return d;
-  }
+    return d;
+}
+
+static List<int> Divisors(int n)
+{
+
+    List<int> r = new List<int>();
+
+    for(int i = 1; i < (int) Math.Sqrt(n); i++)
+    {
+        if(n%i == 0)
+        {
+            r.Add(i);
+            r.Add(n/i);
+        }
+    }
+
+    return r;
+
+}
