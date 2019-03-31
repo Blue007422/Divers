c293f69185450db98f618de7b88542a2f9747c0b
ValentinOp <valentin.copin@gmail.com>
2019-04-01 00:28:02 +0200

    Update MathStuff.cs

--- MathStuff.cs
+++ MathStuff.cs
@@ -1,1 +1,27 @@
 
+static int PGCD(int a, int b)
+  {
+      int c;
+      int d;
+      int r;
+
+      if(a > b)
+      {
+          c = a;
+          d = b;
+      }
+      else
+      {
+          d = a;
+          c = b;
+      }
+
+      while(d != 0 && c%d != 0 && d != 1)
+      {
+          r = c%d;
+          c = d;
+          d = r;
+      }
+
+      return d;
+  }
